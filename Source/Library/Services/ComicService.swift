92a1cfc7944c9d25fe2892759702224c912645b4
Khoa Pham <onmyway133@gmail.com>
2018-11-13 22:32:16 +0100

    Add basic comics service

---
+++ Source/Library/Services/ComicService.swift
@@ -0,0 +1,14 @@
+//
+//  ComicService.swift
+//  Xkcd
+//
+//  Created by khoa on 13/11/2018.
+//  Copyright © 2018 onmyway133. All rights reserved.
+//
+
+import Foundation
+
+// Fetch comics https://xkcd.com/json.html
+final class ComicService {
+
+}
