63d433d97b2e1fb51e0d266aa18a755f7145f59a
Khoa Pham <onmyway133@gmail.com>
2018-11-13 23:15:23 +0100

    Test for fetching current comic

--- XkcdUITests/Services/ComicServiceTests.swift
+++ XkcdUITests/Services/ComicServiceTests.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2018 onmyway133. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Xkcd
+
+class ComicServiceTests: XCTestCase {
+  func testFetchCurrent() {
+    let expectation = self.expectation(description: #function)
+    let mockNetworkService = NetworkServiceMock(fileName: "currentComic")
+    let comicService = ComicService(networking: mockNetworkService)
+    comicService.fetchCurrent(completion: { comic in
+      XCTAssertNotNil(comic)
+      XCTAssertEqual(comic!.title, "title")
+      XCTAssertEqual(comic!.id, 2071)
+      expectation.fulfill()
+    })
+
+    wait(for: [expectation], timeout: 1)
+  }
+}
