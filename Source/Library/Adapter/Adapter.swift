7f8e7290472e3b9a6f7e7aeb6deb7e32e2c61942
Khoa Pham <onmyway133@gmail.com>
2018-11-13 22:13:47 +0100

    Add Adapter for UICollectionView

---
+++ Source/Library/Adapter/Adapter.swift
@@ -0,0 +1,46 @@
+//
+//  Adapter.swift
+//  Xkcd
+//
+//  Created by khoa on 13/11/2018.
+//  Copyright © 2018 onmyway133. All rights reserved.
+//
+
+import UIKit
+
+/// A generic adapter to act as convenient DataSource and Delegate for UICollectionView
+final class Adapter<T, Cell: UICollectionViewCell>: NSObject,
+UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  var items: [T] = []
+  var configure: ((T, Cell) -> Void)?
+  var select: ((T) -> Void)?
+  var cellHeight: CGFloat = 60
+
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let item = items[indexPath.item]
+
+    let cell: Cell = collectionView.dequeue(indexPath: indexPath)
+    configure?(item, cell)
+    return cell
+  }
+
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let item = items[indexPath.item]
+    select?(item)
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+    sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+    return CGSize(width: collectionView.frame.size.width, height: cellHeight)
+  }
+}
