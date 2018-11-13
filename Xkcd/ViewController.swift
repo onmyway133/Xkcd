078a69cc531a5ffa2f274274c4633f5eae596fad
Khoa Pham <onmyway133@gmail.com>
2018-11-13 21:13:32 +0100

    Initial Commit

---
+++ Xkcd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xkcd
+//
+//  Created by khoa on 13/11/2018.
+//  Copyright © 2018 onmyway133. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
